bac386263b884bf6f5b041befdd05707c9e4a88b
Michael Tedder <mike@tedder.cc>
2013-09-16 21:18:15 +0900

    Converted more glenz code

--- source/jni/u2/glenz/glenz-asm.c
+++ source/jni/u2/glenz/glenz-asm.c
@@ -33,18 +33,228 @@ static int16_t ndp0 = 0, ndp = 0;
 static nedata *nec = NULL;
 static nedata *nep[NROWS] = { NULL };
 static nedata  ne[MAXLINES] = { 0 };
-static int8_t  nl[256] = { 0 };
+static nedata *nl[NROWS] = { NULL };
+
+static int16_t yrow = 0, yrowad = 0;
+static nedata **siend = NULL;
+
+// from adata.asm
+static int16_t newdata1[0xFFF0 / 2] = { 0 };		// accessed using 16-bit words
+
+extern char bgpic[65535];
 
 static void ng_init()
 {
+	int16_t ax;
+
+	nec  = &ne[0];
+	ax   = ndp0;
+	ax  ^= 0x8000;
+	ndp0 = ax;
+	ndp  = ax;
+
+	for (ax = 0; ax < NROWS; ax++)
+		nep[ax] = NULL;
+
+	if (newdata1[0] == 0)
+		newdata1[0] = -1;
+
+	if (newdata1[0x4000] == 0)
+		newdata1[0x4000] = -1;
 }
 
 static void ng_pass2()
 {
+	nedata *bx, *dx;
+	nedata **bp, **si, **di, **sbp, **sdi;
+	int cnt, ax, cx;
+
+	bp	   = &nep[0];
+	di	   = &nl[0];
+	cnt	   = 200;
+	yrow   = 0;
+	yrowad = 0;
+
+	// di=pointer to this row list end
+	// bp=pointer to nep
+
+label1:
+	// add new items this row
+	bx = *bp;
+	while (bx != NULL)
+	{
+		*di = bx;
+		di++;
+		bx = bx->next;
+	}
+
+	// sort this row (insertion sort)
+	sbp = bp;
+	sdi = di;
+	bp  = di;
+	si  = &nl[1];
+	goto label4;
+
+label5:
+	// for(k=1;k<listc;k++) {	// k=SI
+	bx = *si;					// bx=list[k]
+	dx = bx;					// dx=i
+	ax = bx->x;					// eax=x
+	di = si;
+	di--;						// di=j
+	goto label6;
+
+label9:
+	// for(j=k-1;j>=0 && x<e[list[j]].x;j--) {
+	di[1] = bx;					// bx=cs:di
+	di--;
+
+label6:
+	if (di >= &nl[0])
+	{
+		bx = *di;
+		if (ax < bx->x)
+			goto label9;
+	}
+
+	di[1] = dx;
+	si++;
+
+label4:
+	if (si < bp)
+		goto label5;
+
+	di = sdi;
+
+	// bp=nl end
+	
+	// process list & kill finished lines
+	siend = bp;
+	cx    = 0x8000;
+	si	  = &nl[0];
+	di	  = si;
+	goto label10;
+
+label11:
+	bx = *si;
+	if (yrow < bx->y2)
+	{
+		*di = bx;
+		di++;
+		ax = bx->x;
+		bx->x += bx->dx;
+		ax >>= 16;
+
+		// clip X
+		if (ax > 319)
+			ax = 319;
+		else if (ax < 1)
+			ax = 1;
+
+		if (cx == ax)
+		{
+			// same x pos
+			ax = bx->color;
+			newdata1[ndp - 1] ^= ax;
+		}
+		else
+		{
+			// new x pos
+			ax += yrowad;
+			newdata1[ndp    ] = ax;
+			ax = bx->color;
+			newdata1[ndp + 1] = ax;
+			ndp += 2;
+			cx = ax;
+		}
+	}
+
+	si++;
+
+label10:
+	if (si < siend)
+		goto label11;
+
+	bp  = sbp;
+	yrow++;
+	yrowad += 320;
+	bp++;
+	
+	cnt--;
+	if (cnt > 0)
+		goto label1;
+
+	newdata1[ndp    ] = 63999;
+	newdata1[ndp + 1] = 0;
+	newdata1[ndp + 2] = -1;
+	newdata1[ndp + 3] = 0;
+	ndp += 4;
+}
+
+static inline void fillmacro(uint8_t *vram, const uint8_t *bg, uint8_t color, int count)
+{
+	while (count--)
+		*vram++ = *bg++ | color;
 }
 
 static void ng_pass3()
 {
+	uint8_t *vram = MK_FP(0xA000, 0x0000);
+	const uint8_t *bg = bgpic;
+	int16_t bx, si;
+	int di, al, ah, dhx, dx, chx, cx;
+
+	di  = 0;
+	si  = ndp0;
+	bx  = si;
+	bx ^= 0x4000;
+	al  = ah = 0;
+
+	//si=new     bx=last
+	//cx=newpos  dx=lastpos
+	//al=newcol  ah=lastcol
+	dhx = newdata1[bx    ];
+	dx  = newdata1[bx + 1];
+	bx += 2;
+	chx = newdata1[si    ];
+	cx  = newdata1[si + 1];
+	si += 2;
+
+label21:
+	if (dx < cx)
+		goto label23;
+	else if (dx == cx)
+		goto label22;
+
+	// cx<dx
+	if (al != ah)
+		fillmacro(vram + di, bg + di, al, cx - di);
+
+	di  = cx;
+	cx  = chx;
+	al ^= cx & 0xFF;
+	chx = newdata1[si    ];
+	cx  = newdata1[si + 1];
+	si += 2;
+	goto label21;
+
+label22:
+	// cx=dx
+	if (cx == -1)
+		return;
+
+label23:
+	// dx<cx
+	if (al != ah)
+		fillmacro(vram + di, bg + di, al, cx - di);
+
+	di  = dx;
+	dx  = dhx;
+	ah ^= dx & 0xFF;
+	dhx = newdata1[bx    ];
+	dx  = newdata1[bx + 1];
+	bx += 2;
+	goto label21;
 }
 
 static void newgroup(int cmd, int *polylist)
